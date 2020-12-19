3c1dfcb9921f2225af867d51b72f0705f55c43e9
CaoMeiYouRen <996881204@qq.com>
2020-12-19 19:09:02 +0800

    chore: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,9 @@
 const IS_PROD = process.env.NODE_ENV === 'production'
 module.exports = {
     root: true,
-    env: {
-        browser: true,
-        es6: true,
-        commonjs: true,
-        node: true,
-        mocha: true,
-    },
     extends: [
         'cmyr/vue3'
     ],
-    plugins: [
-        'vue',
-    ],
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-        ecmaVersion: new Date().getFullYear(),
-        sourceType: 'module',
-        ecmaFeatures: {
-            modules: true,
-        },
-    },
-    parser: 'vue-eslint-parser',
     rules: {
     },
 }
